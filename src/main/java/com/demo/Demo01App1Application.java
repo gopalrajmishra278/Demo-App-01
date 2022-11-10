130cdad45f6c5de147563934b26778ddda67dbcc
Gopalraj Mishra <gopalrajmishra278@gmail.com>
2022-11-10 13:33:54 +0530

    Initial Commit

---
+++ src/main/java/com/demo/Demo01App1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01App1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01App1Application.class, args);
+	}
+
+}
